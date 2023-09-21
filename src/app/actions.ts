e6dec2e9967a09c304263eea63a6de18046e01e0
bolistewardc <bsnunez@espol.edu.ec>
2023-09-21 15:44:45 -0500

    version register sin upload files manejo de datos como json

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,92 +1,77 @@
 //"use server";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { IFormInput } from "@typesApp/index";
+import {
+  FormDataAplicante,
+  IFormInput,
+  formDataFamiliar,
+} from "@typesApp/index";
 
-export const onSubmit: SubmitHandler<IFormInput> = async (
-  data: IFormInput
-) => {
+export const onSubmit: SubmitHandler<IFormInput> = async (data: IFormInput) => {
   try {
-    const formDataAplicante = new FormData();
-    formDataAplicante.append(
-      "nombres",
-      `${data.first_name.toUpperCase()} ${data.second_name.toUpperCase()}`
-    );
-    formDataAplicante.append("apellidos", data.apellidos.toUpperCase());
-    formDataAplicante.append("sexo", data.sexo);
-    formDataAplicante.append(
-      "fecha_nacimiento",
-      data.fecha_nacimiento.toString()
-    );
-    formDataAplicante.append("ciudad_nacimiento", data.ciudad_nacimiento);
-    formDataAplicante.append("pais_nacimiento", data.pais_nacimiento);
-    formDataAplicante.append("pais_residencia", data.pais_residencia);
-    formDataAplicante.append("estado_civil", data.estado_civil);
-    formDataAplicante.append("children", data.children.length.toString());
-    formDataAplicante.append("phone", data.phone);
-    formDataAplicante.append("email", data.email);
-    formDataAplicante.append("address", data.direccion);
-    formDataAplicante.append("passport", data.passport);
-    formDataAplicante.append(
-      "passport_emision",
-      data.passport_emision.toString()
-    );
-    formDataAplicante.append(
-      "passport_expiration",
-      data.passport_expiration.toString()
-    );
-    formDataAplicante.append("education", data.education);
-    formDataAplicante.append("foto", data.foto_aplicante[0] as File);
+    const formDataAplicante: FormDataAplicante = {
+      action: "aplicante",
+      registration_date: "2023-09-20", //urlIMG.split("/")[0],
+      names: `${data.first_name.toUpperCase()} ${data.second_name.toUpperCase()}`,
+      surnames: data.apellidos.toUpperCase(),
+      sexo: data.sexo,
+      birth_date: data.fecha_nacimiento.toString(),
+      birth_city: data.ciudad_nacimiento,
+      birth_country: data.pais_nacimiento,
+      residence_country: data.pais_residencia,
+      civil_status: data.estado_civil,
+      children: data.children.length.toString(),
+      phone: data.phone,
+      email: data.email,
+      address: data.direccion,
+      passport: data.passport,
+      passport_emision: data.passport_emision.toString(),
+      passport_expiration: data.passport_expiration.toString(),
+      education: data.education,
+      foto_url: "url",
+    };
 
-    /*
-    const aplicante_res = await fetch("/api/aplicante", {
-      method: "POST",
-      body: formDataAplicante,
-    });
-    */
-    const example = {"action":"aplicante","registration_date":"2023-09-20","names":"MIGUEL CARLOS3","surnames":"MONTOYA R","sexo":"mujer","birth_date":"2023-09-11","birth_city":"quito","birth_country":"ecuador","residence_country":"Europa","civil_status":"casado","children":"0","phone":"+593 554521215","email":"silaxan2006@hotmail.com","address":"asdfasf","passport":"245896452","passport_emision":"2023-09-11","passport_expiration":"2023-09-12","education":"universidad","foto_url":"2023-09-20/1695230844254-MONTOYA.png"}
-    
-    const aplicante_res = await fetch("https://form.visaglobal.com.ec/register/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json", // Establecer el tipo de contenido a JSON
-      },
-      body: JSON.stringify(example),
-    });
+    const aplicante_res = await fetch(
+      "https://form.visaglobal.com.ec/register/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json", // Establecer el tipo de contenido a JSON
+        },
+        body: JSON.stringify(formDataAplicante),
+      }
+    );
 
     if (!aplicante_res.ok) {
       throw new Error("No se pudo obtener la respuesta de la API");
-    } 
-    
-    console.log(aplicante_res);
-    
-    const { mensaje_ap, id_aplicante } = await aplicante_res.json();
+    }
+
+    const aplicante_data = await aplicante_res.json();
+
+    console.log(aplicante_data.id);
 
     if (data.estado_civil === "casado") {
-      const formDataConyugue = new FormData();
-      formDataConyugue.append(
-        "nombres",
-        `${data.first_name_conyugue.toUpperCase()} ${data.second_name_conyugue.toUpperCase()}`
-      );
-      formDataConyugue.append(
-        "apellidos",
-        data.apellidos_conyugue.toUpperCase()
-      );
-      formDataConyugue.append("passport", data.passport_conyugue);
-      formDataConyugue.append(
-        "passport_emision",
-        data.passport_emision_conyugue.toString()
-      );
-      formDataConyugue.append(
-        "passport_expiration",
-        data.passport_expiration_conyugue.toString()
-      );
-      formDataConyugue.append("foto", data.foto_conyugue[0] as File);
-      formDataConyugue.append("id_aplicante", id_aplicante);
+      const formDataConyugue: formDataFamiliar = {
+        action: "conyugue",
+        registration_date: "2023-09-20",
+        names: `${data.first_name_conyugue.toUpperCase()} ${data.second_name_conyugue.toUpperCase()}`,
+        surnames: data.apellidos_conyugue.toUpperCase(),
+        passport: data.passport_conyugue,
+        passport_emision: data.passport_emision_conyugue.toString(),
+        passport_expiration: data.passport_expiration_conyugue.toString(),
+        id_aplicante: aplicante_data.id,
+        foto_url: "upload",
+      };
 
-      const conyugue_res = await fetch("/api/familiar", {
-        method: "POST",
-        body: formDataConyugue,
-      });
+      const conyugue_res = await fetch(
+        "https://form.visaglobal.com.ec/register/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json", // Establecer el tipo de contenido a JSON
+          },
+          body: JSON.stringify(formDataConyugue),
+        }
+      );
 
       if (!conyugue_res.ok) {
         throw new Error("No se pudo obtener la respuesta de la API");
@@ -94,36 +79,43 @@ export const onSubmit: SubmitHandler<IFormInput> = async (
     }
 
     if (data.children.length > 0) {
-
       const promises = data.children.map(async (kid, index) => {
-        const formDataKid = new FormData();
-        formDataKid.append(
-          "nombres",
-          `${kid.first_name.toUpperCase()} ${kid.second_name.toUpperCase()}`
+
+        const formDataKid: formDataFamiliar = {
+          action: "kid",
+          registration_date: "2023-09-20",
+          names: `${kid.first_name.toUpperCase()} ${kid.second_name.toUpperCase()}`,
+          surnames: kid.apellidos.toUpperCase(),  
+          edad:  kid.edad.toString(),  
+          id_aplicante: aplicante_data.id,
+          foto_url: "upload"//kid.foto_kid[0] as File,
+        }; 
+
+        const kid_res = await fetch(
+          "https://form.visaglobal.com.ec/register/",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json", // Establecer el tipo de contenido a JSON
+            },
+            body: JSON.stringify(formDataKid),
+          }
         );
-        formDataKid.append("apellidos", kid.apellidos.toUpperCase());
-        formDataKid.append("edad", kid.edad.toString());
-        formDataKid.append("foto", kid.foto_kid? kid.foto_kid[0] as File : "foto.jpg");
-        formDataKid.append("id_aplicante", id_aplicante);
-          
-        const kid_res = await fetch("/api/familiar", {
-          method: "POST",
-          body: formDataKid,
-        });
 
         if (!kid_res.ok) {
           // Puedes personalizar el mensaje de error según tus necesidades
           throw new Error(`Error al procesar el niñ@ ${kid.first_name}`);
         }
 
-        return kid_res; 
+        return kid_res;
       });
-      
+
       await Promise.all(promises);
     }
 
-    alert("Enviado exitosamente!. Nos contactaremos mediante el número telefónico del aplicante.");
-
+    alert(
+      "Enviado exitosamente!. Nos contactaremos mediante el número telefónico del aplicante."
+    );
   } catch (error) {
     alert("Error while sending form, please try one more time!");
     console.error("Error al llamar a la API:", error);
